e64909cf2aa7e7a8a20a168c9ec9f27c4bbbd81c
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-28 22:00:20 +0700

    Async Image

--- SwiftUI3.0Test/ContentView.swift
+++ SwiftUI3.0Test/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let url = URL(string: "https://static2.yan.vn/YanNews/2167221/202107/khoanh-khac-hoa-luc-dien-cua-blackpink-khien-nguoi-ham-mo-choang-vang-02cd2c27.jpg")!
+    
     var body: some View {
         NavigationView {
-            
+            VStack {
+                AsyncImage(url: url, scale: 1.0) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } else {
+                        // Error or loading
+                        Image(systemName: "house.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.gray)
+                    }
+                }
+                // There is not aspect ratio working directly
+                // Only frame is working with embeded with clipped
+                // To get original downloeded image
+            }
+            .navigationTitle("Async Image")
         }
     }
 }
